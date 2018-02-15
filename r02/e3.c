cd4bd3135191cb3852875496ded6930aabb5f1bc
PranavSharat <ps3054@nyu.edu>
2018-02-14 20:15:11 -0500

    Update e3.c

--- r02/e3.c
+++ r02/e3.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv) {
     // FIXME: Modify the value of answer so that the output in the end is 1069547520.
-    float answer = 42;
+    float answer = 1.5;
 
     // DO NOT modify following lines.
     unsigned* unsigned_ptr = (unsigned*)(&answer);
