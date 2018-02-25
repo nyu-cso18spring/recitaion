b56cef5f9e57a3ecd9b052f61e47d603ccd50f94
Conrad Christensen <conradbchristensen@gmail.com>
2018-02-25 16:23:52 -0500

    recitation 4 skeleton

---
+++ r04/dlist.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "dlist.h"
+
+// Prepend a value in front of the double list.
+int dlist_prepend(struct dlist** headp, struct dlist** endp, int value) {
+    return -1;
+}
+
+// Append a value in the end of the double list.
+int dlist_append(struct dlist** headp, struct dlist** endp, int value) {
+    return -1;
+}
+
+#if 0
+// To test your implementation, change #if 0 to #if 1.
+// And use this main to debug.
+// Before submitting, remember to change #if 1 back to #if 0. Otherwise,
+// you'll get 0 credit.
+int main(int argc, char **argv)
+{
+    // Your own tests.
+}
+#endif
---
+++ r04/strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// TODO: Complete this API. The argument is a string and the return value is
+//       the length of the string.
+int cso_strlen(char *str) {
+    // Add your code here.
+    return -1;
+}
+
+#if 0
+// To test your implementation, change #if 0 to #if 1.
+// And use this main to debug.
+// Before submitting, remember to change #if 1 back to #if 0. Otherwise,
+// you'll get 0 credit.
+int main(int argc, char **argv)
+{
+    // Your own tests.
+}
+#endif
